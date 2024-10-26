83a55e6c89c8801c930e9681defe215a04b672da
Chandler Ngo <chandlerngo00@gmail.com>
2024-10-25 19:17:25 -0500

    test 1

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 app.get("/api/data", async (req,res) => {
     try{
         // const result = await pool.query('SELECT * FROM container;');
-        res.json({ title: 'Hello from server', body: result.rows });
+        res.json({ title: 'Hello from server' });
     }catch(err){
         console.error(err.message);
         res.status(500).send("Server error");
