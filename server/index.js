071bd9f5bd750b2d8733d829368def885cf9be3c
Chandler Ngo <chandlerngo00@gmail.com>
2024-10-25 19:29:53 -0500

    call db

--- server/index.js
+++ server/index.js
@@ -24,8 +24,8 @@ app.get("/", (req, res) => {
 // template get api request
 app.get("/api/data", async (req,res) => {
     try{
-        // const result = await pool.query('SELECT * FROM container;');
-        res.json({ title: 'Hello from server' });
+        const result = await pool.query('SELECT * FROM container;');
+        res.json({ title: 'Hello from server', body: result.rows });
     }catch(err){
         console.error(err.message);
         res.status(500).send("Server error");
