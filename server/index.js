84f51ee8b4a43f1bfabac757de741d700ffadafd
Chandler Ngo <chandlerngo00@gmail.com>
2024-10-28 12:39:06 -0500

    setup finished

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
 const containerRoutes = require('./routes/containerRoutes');
 
-const app = express;
+app.use(cors());
 
-app.use(express.json());
 app.use('/api', containerRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Server is running.");
+});
+
+app.listen(5000, console.log(`Server started on PORT ${5000}`));
 
 module.exports = app;
