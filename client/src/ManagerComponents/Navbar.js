795fc4f32fb3bfb510ffcfbc08ba5807a2c9c3a6
Ryan Tri <ry4ntr1@gmail.com>
2024-10-30 15:29:40 -0500

    fix: improved Navbar styling, added finishning components

--- client/src/ManagerComponents/Navbar.js
+++ client/src/ManagerComponents/Navbar.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import Logo from "../assets/panda-logo.svg";
+
 import {
 	Drawer,
 	List,
@@ -6,39 +9,94 @@ import {
 	ListItemText,
 	Toolbar,
 	Typography,
+	IconButton,
+	Divider,
+	CssBaseline,
+	Box,
 } from "@mui/material";
 
 function Navbar({ employeeName, selectedSection, onSectionChange }) {
-	const sections = [
-		"Menu",
-		"Inventory",
-		"Orders",
-		"Employees",
-		"Analytics",
-		"Logout",
-		"Switch User",
-	];
+	const sections = ["Menu", "Inventory", "Orders", "Employees", "Analytics"];
+	const [open, setOpen] = useState(false);
+
+	const handleDrawerToggle = () => {
+		setOpen(!open);
+	};
 
 	return (
-		<Drawer variant="permanent">
+		<div>
 			<Toolbar>
-				<Typography variant="h6" noWrap>
-					Welcome, {employeeName}
-				</Typography>
+				<IconButton onClick={handleDrawerToggle}>
+					<MenuIcon />
+				</IconButton>
 			</Toolbar>
-			<List>
-				{sections.map((text) => (
-					<ListItem
-						button
-						key={text}
-						selected={selectedSection === text}
-						onClick={() => onSectionChange(text)}
+
+			<Drawer variant="temporary" open={open} onClose={handleDrawerToggle}>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						height: "100%",
+					}}
+				>
+					<Toolbar>
+						<img src={Logo} alt="Panda Express Logo" />
+					</Toolbar>
+
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+						}}
 					>
-						<ListItemText primary={text} />
-					</ListItem>
-				))}
-			</List>
-		</Drawer>
+						<Typography variant="h6">Welcome, {employeeName}</Typography>
+						<Divider sx={{ width: "100%", my: 2 }} />
+
+						<List>
+							{sections.map((text) => (
+								<ListItem
+									button
+									key={text}
+									selected={selectedSection === text}
+									onClick={() => {
+										onSectionChange(text);
+										handleDrawerToggle();
+									}}
+								>
+									<ListItemText primary={text} />
+								</ListItem>
+							))}
+						</List>
+					</Box>
+
+					<Box sx={{ width: "100%" }}>
+						<Divider />
+						<List>
+							<ListItem
+								button
+								onClick={() => {
+									onSectionChange("Logout");
+									handleDrawerToggle();
+								}}
+							>
+								<ListItemText primary="Logout" />
+							</ListItem>
+							<ListItem
+								button
+								onClick={() => {
+									onSectionChange("Switch User");
+									handleDrawerToggle();
+								}}
+							>
+								<ListItemText primary="Switch User" />
+							</ListItem>
+						</List>
+					</Box>
+				</Box>
+			</Drawer>
+		</div>
 	);
 }
 
